e8b18eec2a7ee3f9e58e5146d50dd2ec5b73a53a
hwyang <qoarhfl@gmail.com>
2023-04-30 03:20:27 +0900

    04 mvc framework

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,38 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="responseHeaderServlet",urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setHeader("Content-Type","text/plain");
+        response.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma","no-cache");
+        response.setHeader("my-header","hello");
+
+        // 편의 메서드
+        content(response);
+        cookie(response);
+        response.getWriter().write("ok");
+    }
+
+    private void content(HttpServletResponse response) {
+        response.setHeader("Content-Type","text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);
+        response.addCookie(cookie);
+    }
+}
